5b29c675b0a10e2e7a16c5a509d64ff7121c3ca3
vivekapadia <vivek610@gmail.com>
2026-02-09 01:25:07 +0530

    chore: migrate to vercel

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     // Base path: '/fintechManager/' for production (GitHub Pages), '/' for local development
-    base: process.env.NODE_ENV === 'production' ? '/fintechManager/' : '/',
+    // base: process.env.NODE_ENV === 'production' ? '/fintechManager/' : '/',
 
     plugins: [react()],
     server: {
